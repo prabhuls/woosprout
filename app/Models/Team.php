2541caedf80220ada89148180e74ae093670a9b7
Prabhul Sankar <prabhul@acnove.com>
2023-05-28 02:28:45 +0530

    team tables and models created

---
+++ app/Models/Team.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'personal_team' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'personal_team',
+    ];
+}
