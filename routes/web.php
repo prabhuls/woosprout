4a4688e39887d89b3b6dccb2af8ebf1b9ae36e1a
Prabhul Sankar <prabhul@acnove.com>
2023-05-29 23:22:00 +0530

    route fix

--- routes/web.php
+++ routes/web.php
@@ -37,16 +37,16 @@ Route::middleware('auth', 'verified')->group(function () {
 //     return view('auth.verify-email');
 // })->middleware('auth')->name('verification.notice');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
  
-    return redirect('/dashboard');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+//     return redirect('/dashboard');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
 
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
  
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 require __DIR__.'/auth.php';
