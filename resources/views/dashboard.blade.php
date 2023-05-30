93d051593f6250fdd3f111ee4c4960e3cd6a5ffa
Prabhul Sankar <prabhul@acnove.com>
2023-05-30 20:57:47 +0530

    remove dark theme

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-user-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-00 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-00 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow border sm:rounded-lg">
-                <div class="p-6 text-gray-700 dark:text-gray-100">
+            <div class="bg-white overflow-hidden shadow border sm:rounded-lg">
+                <div class="p-6 text-gray-700">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
